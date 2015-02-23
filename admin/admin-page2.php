452c589bfbe6b187d0cc3c2ef66a9995ab3956ab
Mukhamad Mukhsin Wibowo <mmwibowo2@gmail.com>
2015-02-23 13:08:49 +0700

    Create admin-page2.php

---
+++ admin/admin-page2.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Admin | 1</title>
+	<link rel="stylesheet" type="text/css" href="../css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="../css/grid.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<header class="main-header">
+		<ul class="grid-container main-nav">
+			<li class="grid-2 logo">
+				<a href="#">
+					<img src="#">
+				</a>
+			</li>
+			<li class="grid-4"></li>
+			<li class="grid-3"><a href="admin-page1.php">Dropdown 2 Dropdown 2</a></li>
+			<li class="grid-3"><a href="admin-page2.php">Dropdown 1 Dropdown 1</a></li>
+		</ul>
+	</header>
+	<section class="grid-container main-content">
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+		<div class="item">
+			Item
+		</div>
+	</section>
+</body>
